af1453733e41e7e5fe09a545818617e558b58806
akhiljuneja3 <akhiljuneja3@users.noreply.github.com>
2020-07-19 14:55:31 +0530

    Update Main.java
    Working code for Finding Nemo and measuring the efficiency

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Arrays;
+
+class DS {
+    public static void main(String[] args) {
+        String[] array1 = new String[1000000];
+        Arrays.fill(array1, "nemo");
+        findNemo(array1);
+        findNemo(array1);
+    }
+
+    public static void findNemo(String[] array){
+        long startTime = System.nanoTime();
+        for(int i=0;i<array.length;i++){
+            if(array[i] == "nemo"){
+                break;
+            }
+        }
+        System.out.println("It took " + (System.nanoTime() - startTime) + " nano seconds to find Nemo.");
+    }
 }
